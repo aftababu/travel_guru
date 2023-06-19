dee9ee77f8439fd9233fbe804a819898897e829e
aftababu <95759557+aftababu@users.noreply.github.com>
2023-06-19 21:03:35 +0600

    Delete firebase.config.js

--- src/contains/Login/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC_-T6VYy52bn5igm1PtWUmpIHHSkO2Sdw",
-  authDomain: "burj-al-arab-bf010.firebaseapp.com",
-  projectId: "burj-al-arab-bf010",
-  storageBucket: "burj-al-arab-bf010.appspot.com",
-  messagingSenderId: "838011901749",
-  appId: "1:838011901749:web:c6ff8546920dde65eb9e7c"
-};
